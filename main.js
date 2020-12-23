019faecde142e6f30e822c348336f5e17d03aae1
kkxop88079 <33884330+kkxop88079@users.noreply.github.com>
2020-12-23 10:58:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function showMobileMenu(){
+    let mobileMenu=document.querySelector("#mobile-menu");
+    mobileMenu.style.display="block";
+}
+function closeMobileMenu(){
+    let closeMenu=document.querySelector("#mobile-menu");
+    closeMenu.style.display="none";
+}
+function overImage3(){
+    let overimage=document.querySelector(".picture3");
+    overimage.style.opacity="100%";
+}
+function outImage3(){
+    let outimage=document.querySelector(".picture3");
+    outimage.style.opacity="75%";
+}
+function overImage4(){
+    let overimage=document.querySelector(".picture4");
+    overimage.style.opacity="100%";
+}
+function outImage4(){
+    let outimage=document.querySelector(".picture4");
+    outimage.style.opacity="75%";
+}
